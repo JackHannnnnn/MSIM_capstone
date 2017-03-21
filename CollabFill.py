75fd7c95db6f3af45ada067205b6b623830d425a
Yao Zhou <yao.zhou@outlook.com>
2017-03-21 08:48:24 -0700

    add basedline score to the algorithmn, exclude the contacted item from recommendation, add a column showing items promoted by emails

--- CollabFill.py
+++ CollabFill.py
@@ -12,9 +12,9 @@ from scipy import sparse
 
 score = sc.calculate_score()
 sc.create_table(score)
-scoreData = dr.DataReader().get_score_data() # read data from the Score table
+dataReader = dr.DataReader()
+scoreData = dataReader.get_score_data()# read data from the Score table
 score_df = scoreData.pivot(index='user_id', columns = 'technology_id', values = 'total_score') # reshape score table 
-
 # calculate the baseline table 
 mu = score_df.stack().mean() # average of total scores
 baseline_df = score_df.copy()
@@ -22,7 +22,7 @@ for user in baseline_df.index:
     for item in baseline_df.columns:
         user_dev = score_df.loc[user].mean() - mu
         item_dev = score_df[item].mean()-mu
-        baseline_df.loc[user,item] = mu + user_dev + item+dev
+        baseline_df.loc[user,item] = mu + user_dev + item_dev
 score_df = score_df.fillna(0) # fill missing values with 0
 
 # get the similarity table
@@ -44,8 +44,8 @@ def rating_predict(user, item):
     # baseline of similar users
     baseline_usr = baseline_df.loc[userls,item] 
     # predict rating of the user on the item
-    if np.sum(sim_scores)!=0:
-        score_pred =  baseline_df.loc[user,item] + np.sum(sim_usr*(sim_scores-baseline_usr))/np.sum(sim_scores) 
+    if np.sum(sim_usr)!=0:
+        score_pred =  baseline_df.loc[user,item] + np.sum(sim_usr*(sim_scores-baseline_usr))/np.sum(sim_usr) 
     else:                                         
         score_pred =  0
     return score_pred
@@ -58,21 +58,23 @@ def rated_users(item):
     userls = score[score!=0].index.values # users that rated the item
     return userls
 
+
 score_pred_df = score_df.copy()
 for user in score_df.index:
     for item in score_df.columns:
         if score_df.loc[user, item]==0.0:            
             score_pred_df.loc[user, item] = rating_predict(user, item)
-print score_pred_df.head(10)
+#print score_pred_df.head()
 
 
-df = pd.DataFrame(index=score_pred_df.index, columns=range(5))
-df.fillna(0)
-for index in score_pred_df.index:
-    pred_item = score_pred_df.loc[index] 
-    df.loc[index] = pred_item.nlargest(5).index.values # find the largest 5 scores for each user
-df.to_csv("User_Based_CF.csv")
+predResult_df = pd.DataFrame(index=score_pred_df.index, columns=range(5))
+for user in score_pred_df.index:
+    contactedItem = dataReader.get_contacted_tech_ids(user)
+    pred_item = score_pred_df.loc[user] 
+    mask = (pred_item.index).isin(contactedItem) # exclude the items user has contacted
+    predResult_df.loc[user] = pred_item[~mask].nlargest(5).index.values # find the largest 5 scores for each user
 
-
+predResult_df["emailed"] = np.array([dataReader.get_emailed_tech_ids(user) for user in predResult_df.index]) # add a column to show emailed items
+predResult_df.to_csv("User_Based_CF.csv")
 
 

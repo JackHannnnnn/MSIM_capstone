85cebeea58c5b132781e9f90e2cf860490e5dcb6
Chaofan Han <fange1206@gmail.com>
2017-04-15 23:16:55 -0700

    Update and ensemble only three results

--- ensemble.py
+++ ensemble.py
@@ -7,21 +7,28 @@ from DataReaderplus import *
 # Read data from individual recommendation result
 cb_interaction = pd.read_csv('cb_interaction.csv', index_col=0)
 cb_self_identified = pd.read_csv('cb_self_identified.csv', index_col=0)
-cb_combination = pd.read_csv('cb_combination.csv', index_col=0)
 item_based_cf = pd.read_csv('item_based_cf.csv', index_col=0)
 
 user_pool = cb_interaction.index.tolist()
 item_pool = cb_interaction.columns.tolist()
 dr = DataReader()
 
-# Preprocess collaborative filatering result
-for row in range(item_based_cf.shape[0]):
-    row_data = item_based_cf.iloc[row]
-    min_val = min([r for r in row_data if r != -float('inf')])
-    max_val = max([r for r in row_data if r != -float('inf')])
-    interval = max_val - min_val
-    item_based_cf.iloc[row] = [(r - min_val) / interval if r != -float('inf') else 0 for r in row_data]
-    
+# Normalize the range of each user's score to [0,1]
+def normalize_df(dataframe):
+    for idx, row in dataframe.iterrows():
+        min_val = min(row)
+        max_val = max(row)
+        interval = max_val - min_val
+        if interval == 0:
+            continue
+        dataframe.ix[idx] = [(r - min_val) / interval for r in row]
+        
+normalize_df(item_based_cf)
+normalize_df(cb_interaction)
+normalize_df(cb_self_identified)
+
+
+# Preprocess collaborative filatering result to make dimensions equal for three data frames
 for item in item_pool:
     if item not in item_based_cf.columns:
         item_based_cf[item] = np.zeros(item_based_cf.shape[0])
@@ -33,30 +40,6 @@ uids = [uid for uid in user_pool if uid not in item_based_cf.index]
 padded_df = pd.DataFrame(np.zeros((len(uids), item_based_cf.shape[1])), index=uids, columns=item_based_cf.columns)
 item_based_cf = pd.concat([item_based_cf, padded_df], axis=0)
 
-# Normalize result form Content-based algorithm
-for row in range(cb_combination.shape[0]):
-    row_data = cb_combination.iloc[row]
-    min_val = min(row_data)
-    max_val = max(row_data)
-    interval = max_val - min_val
-    cb_combination.iloc[row] = [(r - min_val) / interval for r in row_data]
-
-for row in range(cb_interaction.shape[0]):
-    row_data = cb_interaction.iloc[row]
-    min_val = min(row_data)
-    max_val = max(row_data)
-    # print min_val, max_val
-    interval = max_val - min_val
-    cb_interaction.iloc[row] = [(r - min_val) / interval for r in row_data]
-    
-for row in range(cb_self_identified.shape[0]):
-    row_data = cb_self_identified.iloc[row]
-    min_val = min(row_data)
-    max_val = max(row_data)
-    # print min_val, max_val
-    interval = max_val - min_val
-    cb_self_identified.iloc[row] = [(r - min_val) / interval for r in row_data]
-
 
 # Ensemble: optimized weighted average
 item_based_cf.sort_index(0, inplace=True)
@@ -66,12 +49,9 @@ cb_interaction.sort_index(0, inplace=True)
 cb_interaction.sort_index(1, inplace=True)
 cb_self_identified.sort_index(0, inplace=True)
 cb_self_identified.sort_index(1, inplace=True)
-cb_combination.sort_index(0, inplace=True)
-cb_combination.sort_index(1, inplace=True)
 
-weights = [0.65, 0.67, 0.52, 0.57]
-weighted_ensemble = item_based_cf * weights[0] + cb_interaction * weights[1] + cb_self_identified * weights[2] + \
-                    cb_combination * weights[3]    
+weights = [0.65, 0.67, 0.52]
+weighted_ensemble = item_based_cf * weights[0] + cb_interaction * weights[1] + cb_self_identified * weights[2]
 cols = weighted_ensemble.columns
 
 
@@ -90,7 +70,7 @@ if __name__ == '__main__':
     df = pd.DataFrame(recommendations)
     email_clicked = []
     for i, uid in enumerate(uids):
-        email_clicked.append([tech_id  for tech_id in recommendations[i] if tech_id in dr.get_clicked_tech_ids(uid)])
+        email_clicked.append([long(tech_id)  for tech_id in recommendations[i] if long(tech_id) in dr.get_clicked_tech_ids(uid)])
     output = pd.concat([pd.DataFrame({'user_id': uids}), df, pd.DataFrame({'email_clicked': email_clicked})], axis=1)
     output.to_csv('ensemble_recommendations.csv', index=False)
     print 'Time elapsed: ', datetime.now() - time
